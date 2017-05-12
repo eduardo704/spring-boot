e176927809c14031c78e8dee143c71e2993c35a3
Eduardo Vargas <eduardo704@gmail.com>
2017-05-12 15:43:46 +0200

    starting spting boot

---
+++ src/main/java/com/caveofprogramming/App.java
@@ -0,0 +1,36 @@
+package com.caveofprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
+    @ResponseBody
+    @RequestMapping("/")
+    String home() {
+        return "home";
+    }
+
+    @ResponseBody
+    @RequestMapping("/home")
+    String HelloWord() {
+        return "Hello word";
+    }
+
+}
